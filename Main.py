67f4f30db57c347666fa782126f679f9da221ffa
URK21CS2032Jerusha <109647973+URK21CS2032Jerusha@users.noreply.github.com>
2022-10-05 15:30:05 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,8 +18,11 @@ def insert(Hashtable, keyvalue, value) -> None: # Write your code here Hashtable
 # Create Hashtable as a list of list.
 
 HashTable = [[] for _ in range(hash_table_size)]
+ 
+
 input_data = input() data = []
 for item in re.split('], |].', input_data): item = item[1:]
 data = item.split(', ') if len(data) > 1:
 insert(HashTable, int(data[0]), data[1]) display_hash (HashTable)
+
 
